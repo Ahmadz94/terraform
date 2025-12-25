145ccb8176be15cc5c3ec55ff2423e7c5bf944bb
Ahmad <ahmad.zbedaat1208@gmail.com>
2025-12-25 17:04:36 +0200

    add S3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "my-tf-state-bucket"
+    bucket  = "az-tf-bucket-task2"
     key     = "terraform-easy/dev/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
